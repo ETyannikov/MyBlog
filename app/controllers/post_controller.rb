c274748aab6780a10452cee58d89eaddbe2066ec
Eddy Tyannikov <etyannikov@gmail.com>
2020-01-26 00:12:16 +0000

    Added to post controller

--- app/controllers/post_controller.rb
+++ app/controllers/post_controller.rb
@@ -1,19 +1,19 @@
 require './config/environment'
 
-class ApplicationController < Sinatra::Base
+class PostController < Sinatra::Base
 
   configure do
     set :public_folder, 'public'
     set :views, 'app/views'
   end
-
-  get "/" do
-    erb :welcome
-  end
   
-  get "/articles" do 
+  get "/posts" do 
     @posts = Post.all
     erb :'posts/index'
+  end
+  
+  get "posts/new" do
+    erb :'posts/new'
   end
 
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,5 @@
 class Post < ActiveRecord::Base
   belongs_to :user
+  has_many :posts
   belongs_to :posts
 end
